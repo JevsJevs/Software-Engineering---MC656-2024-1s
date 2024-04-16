3fb089485c880cb817909824bb38b1950f1918e4
Mateus Andrade <m230806@dac.unicamp.br>
2024-04-16 17:29:54 -0300

    #6 feat(back-end): provisory chart api

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
 from flask import Flask
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def basePath():
     return "<h1>Hello World - Welcome to our project</h1>"
+
+@app.route("/medals/<country>", methods=["GET"])
+def medals_by_country(country):
+    with open("../lib/Olympics_Medal_Table.csv") as f:
+        table = csv.DictReader(f)
+        for entry in table:
+            if entry["NOC"] == country:
+                return entry
+        return {"error": f"country '{country}' does not exist"}, 404
+            
+@app.route("/medals", methods=["GET"])
+def medals():
+    with open("../lib/Olympics_Medal_Table.csv") as f:
+        table = csv.DictReader(f)
+        return list(table)
