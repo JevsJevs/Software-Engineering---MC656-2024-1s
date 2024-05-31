4f50d86efd820e4e789d51d3a6d7b17de878e9c7
Breno Nishimoto <brenonishimoto@gmail.com>
2024-05-31 19:29:38 -0300

    chore: Restoring mistakely removed code on previous revert commit

---
+++ frontend/src/data/facts_data.js
@@ -0,0 +1,10 @@
+export const facts = [
+    {
+      id: 1,
+      fact: "País com mais medalhas de ouro: EUA"
+    },
+    {
+      id: 2,
+      fact: "Mais de 200 países participam das Olimpíadas"
+    }
+  ];
