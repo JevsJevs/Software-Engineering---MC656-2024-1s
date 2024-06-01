9eb352ee8b0cb2526ebf1d1881000ab8bc1f5273
Gabriel <g222103@dac.unicamp.br>
2024-05-31 22:39:09 -0300

    #29 feat: started migration from csv to database, still much to be done

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import jsonify
 import csv
 import sqlite3
 
-cx = sqlite3.connect("database/teste.db")
+# cx = sqlite3.connect("database/banco.db")
 
 app = Flask(__name__)
 
@@ -21,6 +22,20 @@ def medals_by_country(country):
             
 @app.route("/medals", methods=["GET"])
 def medals():
-    with open("../lib/Olympics_Medal_Table.csv") as f:
-        table = csv.DictReader(f)
-        return list(table)
+    cx = sqlite3.connect("database/banco.db")
+    cur = cx.cursor()
+    cur.execute("""
+        SELECT noc.nome as NOC, 
+               SUM(CASE WHEN medalha.tipo = 'O' THEN 1 ELSE 0 END) as Ouro,
+               SUM(CASE WHEN medalha.tipo = 'P' THEN 1 ELSE 0 END) as Prata,
+               SUM(CASE WHEN medalha.tipo = 'B' THEN 1 ELSE 0 END) as Bronze
+        FROM noc
+        JOIN atleta ON noc.codigo = atleta.noc
+        JOIN medalha ON atleta.id = medalha.atleta
+        GROUP BY noc.nome
+        ORDER BY Ouro DESC, Prata DESC, Bronze DESC
+    """)
+    results = cur.fetchall()
+    cur.close()
+    cx.close()
+    return results
