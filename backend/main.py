114cbee69dd5418f106317db567bef29d958e5bd
Fooxfeir <g223325@dac.unicamp.br>
2024-06-02 15:50:22 -0300

    Refactors main using OOP

--- backend/main.py
+++ backend/main.py
@@ -2,72 +2,89 @@ from flask import Flask
 import csv
 import sqlite3
 
-cx = sqlite3.connect("database/teste.db")
-
 app = Flask(__name__)
 
-@app.route("/")
-def basePath():
-    return "<h1>Hello World - Welcome to our project</h1>"
+
+class MedalTable:
+    file_path = ""
+
+    def get_data(self, unsorted=False):
+        if unsorted:
+            with open(self.file_path) as f:
+                table = csv.DictReader(f)
+            return list(table)
+        else:
+            with open(self.file_path) as f:
+                table = csv.DictReader(f)
+                ranking = sorted(table, key=lambda x: int(x["Gold"]), reverse=True)
+            return ranking
+            
+    def get_ratio(self):
+        with open(self.file_path) as f:
+            table = csv.DictReader(f)
+            ranking = sorted(table, key=lambda x: int(x["Gold"]) / int(x["Total"]), reverse=True)
+        return ranking
+        
+    def get_country(self, country):
+        with open(self.file_path) as f:
+            table = csv.DictReader(f)
+            for entry in table:
+                if entry["NOC"] == country:
+                    return entry
+            return None
+    
+    def medals_by_continent(self, continent):
+        with open(self.file_path) as f:
+            table = csv.DictReader(f)
+            ranking = [entry for entry in table if entry["Continent"] == continent]
+            ranking.append({"Total": sum(int(entry["Total"]) for entry in ranking), 
+                            "Gold": sum(int(entry["Gold"]) for entry in ranking), 
+                            "Silver": sum(int(entry["Silver"]) for entry in ranking), 
+                            "Bronze": sum(int(entry["Bronze"]) for entry in ranking)})
+            return ranking
+        
+class MedalTableCurrent(MedalTable):
+    file_path = "../lib/Olympics_Medal_Table.csv"
+
+class MedalTablePast(MedalTable):
+    file_path = "../lib/Olympics_Medal_Table_Past.csv"
+
+class MedalTableByCategory(MedalTable):
+    file_path = "../lib/Olympics_Medal_Table.csv"
+
+    def get_data(self, category):
+        with open(self.file_path) as f:
+            table = csv.DictReader(f)
+            return [entry for entry in table if entry["Category"] == category]
 
 @app.route("/medals/<country>", methods=["GET"])
 def medals_by_country(country):
-    with open("../lib/Olympics_Medal_Table.csv") as f:
-        table = csv.DictReader(f)
-        for entry in table:
-            if entry["NOC"] == country:
-                return entry
+    querry = MedalTableCurrent().get_country(country)
+    if querry:
+        return querry
+    else:
         return {"error": f"country '{country}' does not exist"}, 404
-            
+
 @app.route("/medals", methods=["GET"])
 def medals():
-    with open("../lib/Olympics_Medal_Table.csv") as f:
-        table = csv.DictReader(f)
-        return list(table)
+    return MedalTableCurrent().get_data(unsorted = True)
 
 @app.route("/medals/top/<int:n>", methods=["GET"])
 def top_n(n):
-    # Retorna os n países com mais medalhas de ouro
-    with open("../lib/Olympics_Medal_Table.csv") as f:
-        table = csv.DictReader(f)
-        ranking = sorted(table, key=lambda x: int(x["Gold"]), reverse=True)
-        return ranking[:n]
-        
+    return MedalTableCurrent().get_data()[:n]
+
 @app.route("/medals/ratio", methods=["GET"])
 def medals_ratio():
-    # Retorna os países ordenados pelo razão de ouro/total
-    with open("../lib/Olympics_Medal_Table.csv") as f:
-        table = csv.DictReader(f)
-        ranking = sorted(table, key=lambda x: int(x["Gold"]) / int(x["Total"]), reverse=True)
-        return ranking
-    
+    return MedalTableCurrent().get_ratio()
+
 @app.route("/medals/compare", methods=["GET"])
 def medals_compare():
-    # Retorna o top 10 (provisoriamente) de medalhas de ouro dos jogos atuais e da última edição
-    with open("../lib/Olympics_Medal_Table.csv") as f:
-        table = csv.DictReader(f)
-        ranking = sorted(table, key=lambda x: int(x["Gold"]), reverse=True)[:10]
-        f.close()
-    with open("../lib/Olympics_Medal_Table_Past.csv") as f:
-        table = csv.DictReader(f)
-        ranking_past = sorted(table, key=lambda x: int(x["Gold"]), reverse=True)[:10]
-        f.close()
-    return {"current": ranking, "past": ranking_past}
+    return {"current": MedalTableCurrent().get_data()[:10], "past": MedalTablePast().get_data()[:10]}
 
-# Daqui pra baixo não funcional apenas com o csv que temos    
 @app.route("/medals/category/<category>", methods=["GET"])
 def medals_by_category(category):
-    with open("../lib/Olympics_Medal_Table.csv") as f: # substituir isso por uma busca no db de medalhas
-        table = csv.DictReader(f)
-        return [entry for entry in table if entry["Category"] == category]
-    
+    return MedalTableByCategory().get_data(category)
+
 @app.route("/medals/continent/<continent>", methods=["GET"])
 def medals_by_continent(continent):
-    with open("../lib/Olympics_Medal_Table.csv") as f: # substituir isso por um join para pegar os continentes
-        table = csv.DictReader(f)
-        ranking = [entry for entry in table if entry["Continent"] == continent]
-        ranking.append({"Total": sum(int(entry["Total"]) for entry in ranking), 
-                        "Gold": sum(int(entry["Gold"]) for entry in ranking), 
-                        "Silver": sum(int(entry["Silver"]) for entry in ranking), 
-                        "Bronze": sum(int(entry["Bronze"]) for entry in ranking)})
-        return ranking
+    return MedalTableCurrent().medals_by_continent(continent)
