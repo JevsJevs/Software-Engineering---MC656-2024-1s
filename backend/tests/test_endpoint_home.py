fa4b8b6fad76c60ca69b2c91c1e96480bde116b0
João Eduardo Vasconcellos da Silva <joao.eduardovsilva@hotmail.com>
2024-06-21 19:24:56 -0300

    test: Remove previous pattern api tests

--- backend/tests/test_endpoint_home.py
+++
@@ -1,8 +0,0 @@
-from main import app
-
-def test_checkHome():
-    instance = app.test_client()
-    resp = instance.get("/")
-    data = resp.get_data()
-    assert resp.status_code == 200
-    assert data == b"<h1>Hello World - Welcome to our project</h1>"
